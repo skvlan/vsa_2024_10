13908712266ece979a463fbe591a2c9f0bc52fef
skvlan <skvland052@gmail.com>
2024-11-11 23:37:31 +0100

    Added docker-compose, postgres and fixed CI

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,3 +1,5 @@
+import os
+
 from config.settings.base import *  # NOQA:
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -10,13 +12,33 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS += ["django_extensions"]  # NOQA:
 
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",  # NOQA:
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "0.0.0.0",
+            "PORT": 5432,
+        },
     }
-}
+
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        },
+        # "default": {
+        #     "ENGINE": "django.db.backends.sqlite3",
+        #     "NAME": BASE_DIR / "db.sqlite3",  # NOQA:
+        # },
+    }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
